1a86817d39b2af55281ba8dcc050f33a3b010797
william-open <lifecliphacker@gmail.com>
2025-09-03 22:17:03 +0800

    更新代码

--- src/api/biz/finance.ts
+++ src/api/biz/finance.ts
@@ -5,3 +5,12 @@ export function financeAccountRecords(params: any) {
     return request.get({ url: '/biz/finance/account_records', params })
 }
 
+// 商户账户余额列表
+export function financeBalance(params: any) {
+    return request.get({ url: '/biz/finance/balance',params })
+}
+
+// 商户货币列表
+export function financeCurrency() {
+    return request.get({ url: '/biz/finance/currency' })
+}
